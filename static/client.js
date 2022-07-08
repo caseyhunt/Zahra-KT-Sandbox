f12e7183acd018918e7b4d1ea833eaba74dca943
caseyhunt <casey.hunt@colorado.edu>
2022-07-08 14:25:22 -0600

    Update client.js

--- static/client.js
+++ static/client.js
@@ -50,7 +50,7 @@ const domain = 'meet.jit.si';
 const options = {
 roomName: 'JitsiMeetAPIExample',
 width: "100%",
-height: "50%",
+height: "100%",
 parentNode: document.querySelector('#meet'),
 lang: 'en',
 };
